2a28b5d6cd40cc3bb85d431c404ad4983beefb7b
Michel Bruchet <michel.bruchet@cea.fr>
2020-10-12 15:58:12 +0200

    Module create offer improving

---
+++ Server/Controllers/OfferController.cs
@@ -0,0 +1,12 @@
+﻿using Abeer.Shared.Functional;
+
+using System.Collections.Concurrent;
+
+namespace Abeer.Server.Controllers
+{
+    public class OfferController
+    {
+        private static readonly ConcurrentBag<OfferModel> Offers = new ConcurrentBag<OfferModel>();
+
+    }
+}
